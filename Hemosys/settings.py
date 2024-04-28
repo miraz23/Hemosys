fded4fdbdd61bfe0b02ffd5d91496938fa5aee64
MOHIUL ISLAM <mohiulislam23@gmail.com>
2024-04-28 19:50:01 +0600

    Added postgreSQL

--- Hemosys/settings.py
+++ Hemosys/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'Hemosys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Hemosys',
+        'USER': 'postgres',
+        'PASSWORD': 'PASSforPOSTGRE8060',
+        'PORT': '5432',
+        'HOST':'localhost',
     }
 }
 
