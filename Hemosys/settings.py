ba9f3e2fc68d3af61a02dcf3380408f2aed473b3
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-05-02 00:10:47 +0600

    Deploy-3

--- Hemosys/settings.py
+++ Hemosys/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'Hemosys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Hemosys',
-        'USER': 'postgres',
-        'PASSWORD': 'PASSforPOSTGRE8060',
-        'PORT': '5432',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Hemosys',
+#         'USER': 'postgres',
+#         'PASSWORD': 'PASSforPOSTGRE8060',
+#         'PORT': '5432',
+#         'HOST':'localhost',
+#     }
+# }
 
 
 # Password validation
@@ -155,14 +155,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
-# import os
+import os
 # STATIC_URL = '/static/'
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
-# MEDIA_URL ='/media/'
-# MEDIA_ROOT =os.path.join(BASE_DIR,"media")
+MEDIA_URL ='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
