e779da640af8aaf0df68e23d3ac88d1bf3416bb8
Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-06-04 13:11:36 +0600

    Deploy-12

--- Hemosys/settings.py
+++ Hemosys/settings.py
@@ -24,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vggk6@=1u4$m=vx$&p+&0zo4em0=u=5bx!l69$!n_*6)!1p%xf'
-# SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-vggk6@=1u4$m=vx$&p+&0zo4em0=u=5bx!l69$!n_*6)!1p%xf')
+# SECRET_KEY = 'django-insecure-vggk6@=1u4$m=vx$&p+&0zo4em0=u=5bx!l69$!n_*6)!1p%xf'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-vggk6@=1u4$m=vx$&p+&0zo4em0=u=5bx!l69$!n_*6)!1p%xf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', '') != 'False'
-
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost', "hemosys.onrender.com"]
 
